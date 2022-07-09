2c6aba936a3365c327cc36e2f535a44013274a34
x0chaoren0s <x0chaoren0s.gmail.com>
2022-07-10 07:02:30 +0800

    --name sshserver123

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 # sudo docker run -it --network=host -v /home/pi/tutorial:/tutorial -w /tutorial easypi/scrapyd scrapy crawl novel
 
 # sshserver1
-sudo docker run -it --network=host -v /home/pi/tutorial:/tutorial -w /tutorial easypi/scrapyd scrapy crawl sshservers1_v3
+sudo docker run -d --network=host --name sshserver1 -v /home/pi/tutorial:/tutorial -w /tutorial easypi/scrapyd scrapy crawl sshservers1_v3
 
 # sshserver2
-# sudo docker run -it --network=host -v /home/pi/tutorial:/tutorial -w /tutorial easypi/scrapyd scrapy crawl sshservers2
+sudo docker run -d --network=host --name sshserver2 -v /home/pi/tutorial:/tutorial -w /tutorial easypi/scrapyd scrapy crawl sshservers2
+
+# sshserver3
+sudo docker run -d --network=host --name sshserver3 -v /home/pi/tutorial:/tutorial -w /tutorial easypi/scrapyd scrapy crawl sshservers3
