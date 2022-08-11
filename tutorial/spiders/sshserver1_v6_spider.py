e36d688f67cb32219799467ecc519efd905e4734
x0chaoren0s <x0chaoren0s@gmail.com>
2022-08-11 11:36:16 +0800

    小更新

---
+++ tutorial/spiders/sshserver1_v6_spider.py
@@ -0,0 +1,105 @@
+import scrapy, time
+from utils.common_tools import getRandStr, GlobalCounter_arr
+from ..items import SshServerProviderHostItem, SshServerConfigItem
+from scrapy.crawler import CrawlerProcess
+
+# scrapy crawl sshservers1_v6
+class SSHServers1Spider_director(scrapy.Spider):
+    name = "sshservers1_v6"
+    fillingForm_interval_secs = 60+2  # 该网站要求60s后再创建一个新用户
+    CRAWLED_IDX = 0 # 用于计数已爬取的有效服务器，配合上述 fillingForm_interval_secs 进行暂停
+    custom_settings = {
+        # 'DOWNLOAD_DELAY': 70, # 该网站要求60s后再创建一个新用户
+        "AUTOTHROTTLE_ENABLED" : True,
+        'AUTOTHROTTLE_DEBUG': True,
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47',
+        'ITEM_PIPELINES' : {
+            'tutorial.pipelines.SshServerWritingJsonPipeline': 300,
+        },
+        'DOWNLOADER_MIDDLEWARES' : {
+            'tutorial.middlewares.TutorialDownloaderMiddleware': 543,
+            'tutorial.middlewares.DeferringDownloaderMiddleware': 544 # 用于使特定的 request 在特定的时间延迟后再发送
+        },
+        'CONCURRENT_REQUESTS' : 1 # default 16 最大并发数，该网站要求每次创建用户前后有固定间隔，因此并发数设为1，间隔时间就不用累加设置
+    }
+
+    def start_requests(self):
+        list_url = 'https://www.mytunneling.com/ssh-server-30' # 仅存储服务器列表页面 url
+        yield scrapy.Request(list_url, self.parse, headers={"referer": 'https://www.mytunneling.com'})
+
+    def parse(self, response):
+        '''
+        爬取服务器列表页面，产生服务器供应商信息，获得各服务器 serverid 和所在区域 region
+        并调用 parse_server_after_fillingForm 爬取各服务器的配置
+        '''
+        provider_host = response.css('a.navbar-brand::text').get()
+        yield SshServerProviderHostItem({
+            'provider_host': provider_host,
+            'list_url'     : response.url
+        })
+
+        # 服务器配置页面 url，但不需要真的访问，只需要从中提取 serverid，
+        # 真正的提交页面并非上述 url，而是 https://www.mytunneling.com/create-account-ssh-30.php
+        servers_urls = response.css('div.row a::attr(href)').getall()
+        servers_ids  = [url.split('/')[-2] for url in servers_urls]
+        # 服务器所在区域
+        servers_regions = response.xpath('//div[@class="row"]//ul/li[2]/text()').getall()
+        servers_regions = [r[16:] for r in servers_regions] # "Server Location Germany" -> "Germany"
+        for (serverid, region) in zip(servers_ids, servers_regions):
+            process = CrawlerProcess()
+            process.crawl('_sshservers1_v6')
+            process.start() # the script will block here until all crawling jobs are finished
+
+            yield scrapy.FormRequest(
+                url='https://www.mytunneling.com/create-account-ssh-30.php',
+                formdata = {'serverid': serverid, 'username': getRandStr(), 'password': getRandStr()},
+                callback = self.parse_server_after_fillingForm,
+                cb_kwargs = {'region': region},
+                meta = {
+                    'request_interval_secs': self.fillingForm_interval_secs if GlobalCounter_arr[self.CRAWLED_IDX].count()>1 else 0,
+                    'cnt_crawled': GlobalCounter_arr[self.CRAWLED_IDX].show()
+                }  # meta 数据用于给 DeferringDownloaderMiddleware 传参
+            )
+
+class SSHServers1Spider_worker(scrapy.Spider):
+    name = "_sshservers1_v6"
+    fillingForm_interval_secs = 60+2  # 该网站要求60s后再创建一个新用户
+    CRAWLED_IDX = 0 # 用于计数已爬取的有效服务器，配合上述 fillingForm_interval_secs 进行暂停
+    custom_settings = {
+        # 'DOWNLOAD_DELAY': 70, # 该网站要求60s后再创建一个新用户
+        "AUTOTHROTTLE_ENABLED" : True,
+        'AUTOTHROTTLE_DEBUG': True,
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47',
+        'ITEM_PIPELINES' : {
+            'tutorial.pipelines.SshServerWritingJsonPipeline': 300,
+        },
+        'DOWNLOADER_MIDDLEWARES' : {
+            'tutorial.middlewares.TutorialDownloaderMiddleware': 543,
+            'tutorial.middlewares.DeferringDownloaderMiddleware': 544 # 用于使特定的 request 在特定的时间延迟后再发送
+        },
+        'CONCURRENT_REQUESTS' : 1 # default 16 最大并发数，该网站要求每次创建用户前后有固定间隔，因此并发数设为1，间隔时间就不用累加设置
+    }
+
+    def start_requests(self):
+        list_url = 'https://www.mytunneling.com/ssh-server-30' # 仅存储服务器列表页面 url
+        yield scrapy.Request(list_url, self.parse, headers={"referer": 'https://www.mytunneling.com'})
+    def parse_server_after_fillingForm(self, response, region):
+        ''' 爬取注册账户后服务器的配置信息 '''
+        body_strlist = response.xpath('//text()').getall()
+        if len(body_strlist) == 1:
+            return SshServerConfigItem({
+                'error_info': body_strlist[0]
+            })
+
+        def normalize_date(datestr): # 如把 05-June-2022 标准化成 2022-06-05
+            return time.strftime("%Y-%m-%d",time.strptime(datestr,"%d-%B-%Y"))
+        return SshServerConfigItem({
+            'region':       region,
+            'username':     body_strlist[1],
+            'password':     body_strlist[4].split(':')[1].split()[0],
+            'host':         body_strlist[5].split(':')[1].split()[0],
+            'date_created': normalize_date(body_strlist[6].split(':')[1].split()[0]),
+            'date_expired': normalize_date(body_strlist[7].split(':')[1].split()[0]),
+            'max_logins':   body_strlist[-5].split()[6][1:-1]
+        })
+
---
+++ tutorial/spiders/sshserver5_spider.py
@@ -0,0 +1,132 @@
+import scrapy, time, os, platform
+from utils.common_tools import getRandStr, GlobalCounter, normalized_local_date, normalize_date
+from ..items import SshServerProviderHostItem, SshServerConfigItem
+from utils.ReCaptcha_Solvers import ReCaptcha_v2_Solver
+
+# scrapy crawl sshservers5
+class SSHServers5Spider(scrapy.Spider):
+    name = "sshservers5"
+    base_url = "https://serverssh.net"
+    # fillingForm_interval_secs = 60*5+2 # 该网站要求 5 min 后才能创建下一个新用户
+    
+    custom_settings = {
+        # "AUTOTHROTTLE_ENABLED" : True,
+        # 'AUTOTHROTTLE_DEBUG': True,
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47',
+        'ITEM_PIPELINES' : {
+            'tutorial.pipelines.SshServerWritingJsonPipeline': 300,
+        },
+        'DOWNLOADER_MIDDLEWARES' : {
+            'tutorial.middlewares.TutorialDownloaderMiddleware': 543,
+            # 'tutorial.middlewares.DeferringDownloaderMiddleware': 544 # 用于使特定的 request 在特定的时间延迟后再发送
+        },
+        'CONCURRENT_REQUESTS' : 1 # default 16 最大并发数，该网站要求每次创建用户前后有固定间隔，因此并发数设为1，间隔时间就不用累加设置
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(self.base_url, self.parse)
+
+    def parse(self, response):
+        '''
+        爬取服务器组列表页面，该列表有6项可选：3days,7days,30days,openvpn,ss,v2ray。此处选 7days（30days抢不到服务器）
+        并调用 parse_server_list 爬取服务器列表页面信息。该列表不是总列表，一页最多列出2个服务器，需要点下一页
+        '''
+        server_group_heads_urls = response.xpath('//div[@class="col-lg-4 col-md"]//a/@href').getall()
+        # print(server_group_heads_urls)
+        init_server_list_url = self.base_url + server_group_heads_urls[1]
+        yield SshServerProviderHostItem({
+            'provider_host': 'serverssh.net',
+            'list_url'     : init_server_list_url
+        })
+        yield scrapy.Request(init_server_list_url, self.parse_server_list, headers={"referer":response.url})
+
+    def parse_server_list(self, response):
+        '''
+        ①爬取服务器列表页面信息（一页最多两个服务器），即每个服务器的可用性（available）（Remaining：数字）、填表页面 url
+          调用 parse_server_before_fillingForm 进行填表
+        ②爬取下一页服务器列表页面的url
+          调用 parse_server_list 继续爬取下一页
+        '''
+        server_htmls = response.xpath('//div[@class="col-lg-4 col-md"]').getall()
+        server_seletors = [scrapy.Selector(text=html) for html in server_htmls]
+        server_urls = [s.xpath('//a[@class="btn btn-sm btn-primary hover-translate-y-n3 hover-shadow-lg mb-3"]/@href').get()
+            for s in server_seletors] # ['/?q=create-ssh&filter=5', '/?q=create-ssh&filter=6']
+        server_urls = [self.base_url+url for url in server_urls] # ['https://serverssh.net/?q=create-ssh&filter=5', 'https://serverssh.net/?q=create-ssh&filter=6']
+        server_remainings = [s.xpath('//li[@class="list-group-item py-2"]/text()').getall()[3]
+            for s in server_seletors] # ['Remaining: 0 From 15', 'Remaining: '] # 若有服务器可用，则 'Remaining: ' 下面还有一个元素专门显示有多少个
+        server_availables = [r=='Remaining: ' for r in server_remainings] # [False, True]
+        server_regions = [s.xpath('//li[@class="list-group-item py-2"]/text()').getall()[0]
+            for s in server_seletors] # ['Location: Singapore', 'Location: Singapore']
+        server_regions = [r.split('Location: ')[1] for r in server_regions] # ['Singapore', 'Singapore']
+        server_hosts = [s.xpath('//li[@class="list-group-item py-2"][1]/span/text()').get().strip()
+            for s in server_seletors] # ['SGX1', 'SGX2']
+        server_hosts = [h.lower()+'.serverssh.net' for h in server_hosts] # ['sgx1.serverssh.net', 'sgx2.serverssh.net']
+
+        next_server_list_btn = response.xpath('//ul[@class="pagination justify-content-center"]/li[last()]')
+        if 'disabled' not in next_server_list_btn.xpath('@class').get():
+            next_server_list_url = self.base_url + '/' + next_server_list_btn.xpath('a/@href').get()
+            yield scrapy.Request(next_server_list_url, self.parse_server_list, headers={"referer":response.url})
+
+        for i,available in enumerate(server_availables):
+            print('++++++++++++ server info ++++++++++++++')
+            print(f'region   : {server_regions[i]}')
+            print(f'host     : {server_hosts[i]}')
+            print(f'available: {available}')
+            print('+++++++++++++++++++++++++++++++++++++++')
+            if available:
+                yield scrapy.Request(
+                    server_urls[i], 
+                    self.parse_server_before_fillingForm, 
+                    headers={"referer":response.url},
+                    meta = {
+                        'region': server_regions[i], # 该网站注册成功页面没有服务器的地区信息，因此在这里传参过去
+                        # 'request_interval_secs': self.fillingForm_interval_secs,  # 用于给 DeferringDownloaderMiddleware 传参
+                        # 'cnt_crawled': GlobalCounter_arr[self.CRAWLED_IDX].show() # 用于给 DeferringDownloaderMiddleware 传参
+                    }  
+                )
+            else: # available == False
+                yield SshServerConfigItem({
+                    'region'          : server_regions[i],
+                    'host'            : server_hosts[i],
+                    'error_info'      : 'no available'
+                })
+
+
+    def parse_server_before_fillingForm(self, response):
+        ''' 填表以及通过 recaptcha '''
+        websiteKey = response.xpath('//div[@class="g-recaptcha"]/@data-sitekey').get()
+        recaptcha_res = ReCaptcha_v2_Solver()(response.url, websiteKey)
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={
+                'id': response.url.split('=')[-1],
+                'username': getRandStr(),
+                'password': getRandStr(),
+                'g-recaptcha-response': recaptcha_res,
+                'createAcc': ''
+            },
+            callback=self.parse_server_after_fillingForm,
+            meta = {
+                'region': response.meta['region'], # 该网站注册成功页面没有服务器的地区信息，因此从 parse_server_list 传参过去
+            }
+        )
+        
+    def parse_server_after_fillingForm(self, response):
+        ''' 爬取注册账户后服务器的配置信息 '''
+        if platform.system() != 'Windows': # windows 没有 time.tzset()，但是 windows 一般时区是正确的，不用设置
+            os.environ['TZ']='GMT-8' # 设置成中国所在的东八区时区
+            time.tzset()
+        try:
+            success_info = response.xpath('//li[@class="list-group-item py-3"]/font/b/text()').getall()
+            return SshServerConfigItem({
+                'region'          : response.meta['region'],
+                'username'        : success_info[1].split(':')[-1].strip(),
+                'password'        : success_info[2].split(':')[-1].strip(),
+                'host'            : success_info[0].split(':')[-1].strip(),
+                'date_created'    : normalized_local_date(), # 这个网址不显示账户的注册时间，所以自己填。但其实不太准确，因为不知道网站的显示的到期时间是用什么时区
+                'date_expired'    : normalize_date(success_info[3].split(':')[-1].strip(), '%d-%m-%Y'), # 该网站日期格式 04-08-2022
+                'max_logins'      : '1' # 该网站 3days 最大设备数为 2, 7days 最大设备数为 1
+            })
+        except:
+            with open(f'{GlobalCounter.count()}.html', 'wb') as f:
+                f.write(response.body)
