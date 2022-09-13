0918418881c1df09ad97e79d637fb19222df2fa3
x0chaoren0s <x0chaoren0s@gmail.com>
2022-09-13 21:37:37 +0800

    新增shell调试代码

--- tutorial/spiders/tmp_spider.py
+++ tutorial/spiders/tmp_spider.py
@@ -8,3 +8,12 @@ class TmpSpider(scrapy.spiders.CrawlSpider):
 
     def parse(self, response):
         yield response
+
+# class Self:
+#     def __init__(self) -> None:
+#         self.name = "sshservers4"
+#         self.base_url = "https://www.vpnjantit.com"
+#         self.list_url = 'https://www.vpnjantit.com/free-ssh-7-days'
+# self=Self()
+# fetch(self.list_url)
+# fetch(server_showIP_urls[i], headers={"referer":response.url})
